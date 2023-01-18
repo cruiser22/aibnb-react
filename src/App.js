ed12f5cb188297495e8aee59c54c368ecd60cde8
Paul Vinchent <paulvinchent@Pauls-Air.numericable.fr>
2023-01-18 07:04:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+       {/* Navbar */}
+       {/* filters */}
+       {/* Rentals */}
+       {/* Footers */}
+       </div>
+  );
+}
+
+export default App;
